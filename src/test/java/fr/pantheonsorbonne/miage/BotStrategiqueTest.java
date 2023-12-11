dea8a38e3b1d6bfb7a630559d25ce13c69b91e1f
iblamesro <sara0301mba@yahoo.fr>
2023-12-11 22:31:50 +0100

    version 22h30

--- src/test/java/fr/pantheonsorbonne/miage/BotStrategiqueTest.java
+++ src/test/java/fr/pantheonsorbonne/miage/BotStrategiqueTest.java
@@ -2,6 +2,7 @@ package fr.pantheonsorbonne.miage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
